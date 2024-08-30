8a3844c1b469dedeb9709d8c506492daad7ffe79
noahberthusen <nfberthusen@gmail.com>
2024-08-30 15:53:02 -0400

    minimal working product

---
+++ code_distance.g
@@ -0,0 +1,22 @@
+# C:\Program Files\GAP-v4.12.2\runtime\opt\gap-v4.12.2\pkg\qdistrnd-0.9.2\qdistrnd-0.9.2\matrices
+
+LoadPackage("QDistRnd");
+
+filedir := DirectoriesPackageLibrary("QDistRnd","matrices");;
+# output := OutputTextFile(Filename(filedir, "distances.txt"), false);;
+# SetPrintFormattingStatus(output, false);
+
+qx_filename := "QX.mtx";;
+qz_filename := "QZ.mtx";;
+
+lisX:=ReadMTXE(Filename(filedir, qx_filename), 0);;
+lisZ:=ReadMTXE(Filename(filedir, qz_filename), 0);;
+GX:=lisX[3];;
+GZ:=lisZ[3];;
+dz := DistRandCSS(GX,GZ,10000,0,0);;
+
+# PrintTo(output, i);
+# PrintTo(output, ",");
+# PrintTo(output, dz);
+# PrintTo(output, "\n");
+Print(dz);
---
+++ find_distance.py
@@ -0,0 +1,84 @@
+from classical_code import read_code
+import numpy as np
+import os
+import galois
+
+codes = [
+    "12_9_3_4",
+    "16_12_3_4",
+    "20_15_3_4",
+    "40_30_3_4",
+    "60_45_3_4"
+
+]
+distances = []
+
+def par2gen(H):
+    GF = galois.GF(2)
+    gfH = GF(H)
+    gfH_rank = np.linalg.matrix_rank(gfH)
+
+    rref_H = gfH.row_reduce()
+
+    swaps = []
+    col_H = rref_H.copy()
+    for i in range(gfH_rank):
+        inds = np.where(col_H[i])[0]
+        pivot = inds[0]
+        col_H[:,[i,pivot]] = col_H[:,[pivot,i]]
+        swaps.append((i,pivot))
+
+    col_H = col_H[:gfH_rank]
+    col_G = GF(np.hstack([col_H[:,gfH_rank:].T, np.eye(H.shape[1]-gfH_rank, dtype=int)]))
+
+    G = col_G.copy()
+    for swap in swaps[::-1]:
+        G[:,[swap[1],swap[0]]] = G[:,[swap[0],swap[1]]]
+
+    if (np.any(G @ rref_H[:gfH_rank].T) or np.any(col_G @ col_H.T)):
+        print("FAILED")
+        return
+    return (np.array(G, dtype=int), np.array(col_G, dtype=int))
+
+
+full_path = os.path.realpath(__file__)
+path, filename = os.path.split(full_path)
+
+num_iters = 1000
+GF = galois.GF(2)
+for i, code in enumerate(codes):
+    file = os.path.join(path, f'./codes/{code}.code')
+    ccode = read_code(file)
+    d = ccode.n
+    n = ccode.n
+
+    H = np.zeros((ccode.m, ccode.n), dtype=int)
+    for j, row in enumerate(ccode.check_nbhd):
+        for r in row:
+            H[j][r] = 1
+    k = n - np.linalg.matrix_rank(GF(H))
+
+    hx1 = np.kron(H, np.eye(H.shape[1], dtype=bool))
+    hx2 = np.kron(np.eye(H.shape[0], dtype=bool), H.T)
+    Hx = np.hstack([hx1, hx2])
+
+    hz1 = np.kron(np.eye(H.shape[1], dtype=bool), H)
+    hz2 = np.kron(H.T, np.eye(H.shape[0], dtype=bool))
+    Hz = np.hstack([hz1, hz2])
+
+    G, col_G = par2gen(H)
+
+    for j in range(1, 2**k):
+        num = bin(j)[2:].zfill(k)
+        w = np.array([int(b) for b in num])
+        encoded_w = w@G % 2
+
+        # if (np.any(H@encoded_w)):
+        #     print('Not a codeword')
+        if (np.count_nonzero(encoded_w) < d):
+            d = np.count_nonzero(encoded_w)
+
+    distances.append(d)
+    print(f"{code}: [[{ccode.n},{ccode.n-np.linalg.matrix_rank(GF(H))},{d}]]")
+
+# print(distances)
