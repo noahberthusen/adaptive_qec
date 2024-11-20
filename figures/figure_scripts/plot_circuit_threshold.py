6672fe4f812ca5d95b684559549f584b413106ea
noahberthusen <nfberthusen@gmail.com>
2024-11-19 22:40:59 -0500

    meeting

--- figures/figure_scripts/plot_circuit_threshold.py
+++ figures/figure_scripts/plot_circuit_threshold.py
@@ -25,8 +25,8 @@ codes = [
 
 codes2 = [
     "HGP_100_4",
-    # "HGP_400_16",
-    # "HGP_900_36",
+    "HGP_400_16",
+    "HGP_900_36",
     # "HGP_1600_64",
 ]
 
@@ -54,26 +54,26 @@ ax0_twin.set_yscale('log')
 ax2_twin.get_shared_y_axes().join(ax0_twin, ax1_twin, ax2_twin)
 
 
-# for i, code in enumerate(codes):
-#     df = pd.read_csv(os.path.join(path, f'../../results/{folder}/nonadaptive/{code}.qcode.res'))
-#     df['p_error'] = 1 - df['p_log']
-#     df['p_std_dev'] = np.sqrt(df['p_error'] * df['p_log'] / df['num_test'])
-#     # df['p_error'].replace(to_replace=0, value=1e-4, inplace=True)
-#     df['ler_per_round'] = 1 - (1 - df['p_error'])**(1/df['r'])
-#     df['error_bars'] = (1 - df['p_error'])**(1/(df['r']-1)) * df['p_std_dev'] / df['r']
+for i, code in enumerate(codes):
+    df = pd.read_csv(os.path.join(path, f'../../results/{folder}/nonadaptive/{code}.qcode.res'))
+    df['p_error'] = 1 - df['p_log']
+    df['p_std_dev'] = np.sqrt(df['p_error'] * df['p_log'] / df['num_test'])
+    # df['p_error'].replace(to_replace=0, value=1e-4, inplace=True)
+    df['ler_per_round'] = 1 - (1 - df['p_error'])**(1/df['r'])
+    df['error_bars'] = (1 - df['p_error'])**(1/df['r']-1) * df['p_std_dev'] / df['r']
 
-#     # colors = ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd', '#8c564b', '#e377c2', '#7f7f7f', '#bcbd22', '#17becf']
-#     # tmp_df = df[(df['adapt'] == 1)]
-#     tmp_df = df[(df['p_std_dev'] > 0)]
+    # colors = ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd', '#8c564b', '#e377c2', '#7f7f7f', '#bcbd22', '#17becf']
+    # tmp_df = df[(df['adapt'] == 1)]
+    tmp_df = df[(df['p_std_dev'] > 0)]
 
-#     ax[2].errorbar(tmp_df['p_phys'], tmp_df['ler_per_round'], tmp_df['error_bars'], fmt='o', markersize=4, elinewidth=1.5,
-#                    label=labels[i])
-#     ax2_twin.plot(tmp_df['p_phys'], tmp_df['num_CNOTs'], linestyle="--")
+    ax[2].errorbar(tmp_df['p_phys'], tmp_df['ler_per_round'], tmp_df['error_bars'], fmt='o-', markersize=4, elinewidth=1.5,
+                   label=labels[i])
+    ax2_twin.plot(tmp_df['p_phys'], tmp_df['num_CNOTs'], linestyle="--")
 
-#     ax[2].set_title('nonadaptive [[4,2,2]]-HGP codes')
-#     ax[2].legend(loc='lower right')
-#     ax[2].set_yscale('log')
-#     ax[2].set_xscale('log')
+    ax[2].set_title('nonadaptive [[4,2,2]]-HGP codes')
+    ax[2].legend(loc='lower right')
+    ax[2].set_yscale('log')
+    ax[2].set_xscale('log')
 
 
 
@@ -83,12 +83,12 @@ for i, code in enumerate(codes2):
     df['p_std_dev'] = np.sqrt(df['p_error'] * df['p_log'] / df['num_test'])
     df['p_error'].replace(to_replace=0, value=1e-4, inplace=True)
     df['ler_per_round'] = 1 - (1 - df['p_error'])**(1/df['r'])
-    df['error_bars'] = (1 - df['p_error'])**(1/(df['r']-1)) * df['p_std_dev'] / df['r']
+    df['error_bars'] = (1 - df['p_error'])**(1/df['r']-1) * df['p_std_dev'] / df['r']
 
     # colors = ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd', '#8c564b', '#e377c2', '#7f7f7f', '#bcbd22', '#17becf']
     # tmp_df = df[(df['p_std_dev'] > 0)]
     tmp_df = df
-    ax[1].errorbar(tmp_df['p_phys'], tmp_df['ler_per_round'], tmp_df['error_bars'], fmt='o', markersize=4, elinewidth=1.5,
+    ax[1].errorbar(tmp_df['p_phys'], tmp_df['ler_per_round'], tmp_df['error_bars'], fmt='o-', markersize=4, elinewidth=1.5,
                    label=labels2[i])
     ax1_twin.plot(tmp_df['p_phys'], tmp_df['num_CNOTs'], linestyle="--")
 
@@ -105,14 +105,14 @@ for i, code in enumerate(codes):
     df['p_std_dev'] = np.sqrt(df['p_error'] * df['p_log'] / df['num_test'])
     # df['p_error'].replace(to_replace=0, value=1e-4, inplace=True)
     df['ler_per_round'] = 1 - (1 - df['p_error'])**(1/df['r'])
-    df['error_bars'] = (1 - df['p_error'])**(1/(df['r']-1)) * df['p_std_dev'] / df['r']
+    df['error_bars'] = (1 - df['p_error'])**(1/df['r']-1) * df['p_std_dev'] / df['r']
 
     # colors = ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd', '#8c564b', '#e377c2', '#7f7f7f', '#bcbd22', '#17becf']
 
     tmp_df = df
     tmp_df = df[(df['p_std_dev'] > 0)]
 
-    ax[0].errorbar(tmp_df['p_phys'], tmp_df['ler_per_round'], tmp_df['error_bars'], fmt='o', markersize=4, elinewidth=1.5,
+    ax[0].errorbar(tmp_df['p_phys'], tmp_df['ler_per_round'], tmp_df['error_bars'], fmt='o-', markersize=4, elinewidth=1.5,
                    label=labels[i])
     ax0_twin.plot(tmp_df['p_phys'], tmp_df['num_CNOTs'], linestyle="--")
     ax[0].set_title('[[4,2,2]]-HGP codes')
@@ -127,7 +127,7 @@ ax[0].set_ylabel(r"Logical error rate, $p_{\log}$")
 ax[2].set_xlabel(r"Error rate, $p$")
 ax[1].set_xlabel(r"Error rate, $p$")
 ax[0].set_xlabel(r"Error rate, $p$")
-ax2_twin.set_ylabel("CNOT count")
+ax2_twin.set_ylabel("Average CNOT gates per round")
 
 # plt.show()
 plt.savefig(os.path.join(path, "../memory_circuit.png"), dpi=600, bbox_inches="tight")
